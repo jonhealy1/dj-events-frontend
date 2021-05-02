b550826b0628b161c6a1052c953b22722e695a02
jonhealy1 <jonathan.d.healy@gmail.com>
2021-05-01 20:17:11 -0700

    add layout component

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
+import Layout from '../components/Layout'
+
 export default function AboutPage() {
     return (
-        <div>
+        <Layout title='About DJ Events'>
             <h1>About</h1>
             <p>
                 This is an app to find the latest DJ and
@@ -9,7 +11,7 @@ export default function AboutPage() {
             <p>
                 Version: 1.0.0
             </p>
-        </div>
+        </Layout>
     )
 }
 

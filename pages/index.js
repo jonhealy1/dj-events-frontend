9a26d7600400772bf203edfbe6b3c2be03b5ac66
jonhealy1 <jonathan.d.healy@gmail.com>
2021-05-02 18:46:05 -0700

    add fix routing

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout'
+import {API_URL} from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>
-        Home
+        Upcoming Events
       </h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map(evt => (
+        <h3 key={evt.id}>{evt.name}</h3>
+      ))}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1
+  }
 }
